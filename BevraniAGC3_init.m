85e45f3945abbfe5de49ae9077f116a141591212
Yee Wei Law <ywlaw@unimelb.edu.au>
2012-06-13 21:28:39 +1000

    Added demand1.mat and demand2.mat and support for UFLS.

--- BevraniAGC3_init.m
+++ BevraniAGC3_init.m
@@ -11,3 +11,19 @@ B1 = 0.3483;    B2 = 0.3827;    B3 = 0.3692;
 T12 = 0.20;     T21 = 0.20;     
 T13 = 0.25;     T31 = 0.25;
 T23 = 0.12;     T32 = 0.12;
+
+% Demand profiles
+load('demand1.mat')
+load('demand2.mat')
+
+% Underfrequency load shedding
+% See P. M. Anderson et al., "An adatpive method for setting underfrequency
+% load shedding relays," IEEE Transactions on Power Systems 7(2):647-655, 1992
+global totalLoad2Shed firstLoad2Shed finalLoad2Shed uflsMinDeltaF 
+
+Pstep = 1;
+staticLoad2Shed = Pstep - 0.1042*H1; % Static Load Shed in p.u.
+totalLoad2Shed = 1.05*staticLoad2Shed; % Dynamic Load Shed in p.u.
+firstLoad2Shed = staticLoad2Shed/2;
+finalLoad2Shed = mod(totalLoad2Shed-firstLoad2Shed, 0.1);
+uflsMinDeltaF = -0.8-0.3*floor((totalLoad2Shed-firstLoad2Shed)/0.1);
