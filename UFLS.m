854a20e105afb65dccb7a9567223c26e688221c4
Yee Wei Law <ywlaw@unimelb.edu.au>
2012-06-13 11:48:31 +1000

    lastLoad2Shed to finalLoad2Shed.

--- UFLS.m
+++ UFLS.m
@@ -2,7 +2,7 @@ function toShed = UFLS(t, deltaf)
 % See P. M. Anderson et al., "An adatpive method for setting underfrequency
 % load shedding relays," IEEE Transactions on Power Systems 7(2):647-655, 1992
 
-global totalLoad2Shed firstLoad2Shed lastLoad2Shed uflsMinDeltaF 
+global totalLoad2Shed firstLoad2Shed finalLoad2Shed uflsMinDeltaF 
 persistent uflsOn lastShed startTime
 
 toShed = 0;
@@ -30,8 +30,8 @@ elseif -0.8 < deltaf && deltaf <= -0.5
   end
 elseif deltaf <= uflsMinDeltaF
   if uflsOn == true
-    if totalLoad2Shed > lastShed + lastLoad2Shed
-      toShed = lastShed + lastLoad2Shed;
+    if totalLoad2Shed > lastShed + finalLoad2Shed
+      toShed = lastShed + finalLoad2Shed;
       lastShed = toShed;
       fprintf('[%10.2f] Final stage: %f\n', t, toShed)
     end
