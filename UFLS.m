3008eb10e606f09dd0e62110241ed372fecd1266
Yee Wei Law <ywlaw@unimelb.edu.au>
2012-06-14 11:34:47 +1000

    Working version of Mullen's scheme.

--- UFLS.m
+++ UFLS.m
@@ -1,69 +1,55 @@
-function toShed = UFLS(t, deltaf)
-% See P. M. Anderson et al., "An adatpive method for setting underfrequency
-% load shedding relays," IEEE Transactions on Power Systems 7(2):647-655, 1992
+% Interpreted MATLAB function block for Simulink.
+% Sampling time of this Simulink block should be set to 0.05s.
+function toShed = UFLS(t, deltaf, deltaPest)
 
-global totalLoad2Shed firstLoad2Shed
-persistent stage % 0: no UFLS; 1: first; 2: intermediate; 3: final
-persistent lastShed lastUnstableTime lastStableTime
+global deltaPsafe
+persistent timer shedSoFar scheduledShed lastUnstableTime
 
 if t <= 100
-  stage = 0;
-  toShed = 0;
-  lastShed = 0;
+  timer = 0;
+  shedSoFar = 0;
+  scheduledShed = 0;
   lastUnstableTime = 0;
-  lastStableTime = 0;
+  toShed = 0;
   return
 end
 
-if -0.5 < deltaf
-  if stage > 0 && deltaf < 0.5 && t - lastUnstableTime >= 10
-    stage = 0;
-    toShed = lastShed - 0.1;
-    lastShed = toShed;
-    fprintf('[%10.2f] Load reconnected1: %f\n', t, toShed)
-    lastStableTime = t;
-    return
+if timer > 0
+  timer = timer - 1;
+  if timer == 0
+    shedSoFar = shedSoFar + scheduledShed;
+    toShed = shedSoFar;
+    fprintf('[%8.2f] Shed %f\n', t, toShed)
+  else
+    toShed = shedSoFar;
   end
-  if stage == 0
-    if lastShed >= 0.1
-      toShed = lastShed - 0.1;
-      lastShed = toShed;
-      fprintf('[%10.2f] Load reconnected2: %f\n', t, toShed)
-      return
-    elseif lastShed > 0
-      toShed = 0;
-      lastShed = 0;
-      fprintf('[%10.2f] Load reconnected3: %f\n', t, toShed)
-      return
-    end
-  end
-elseif deltaf <= -0.5% && t - lastStableTime >= 1
-  lastUnstableTime = t;
-  if stage == 0    
-    if ~(lastShed > 0)
-      stage = 1;
-      toShed = firstLoad2Shed;
-      lastShed = toShed;
-      fprintf('[%10.2f] First stage: %f\n', t, toShed)
-      return
-    end
-  end
-elseif delta <= -0.8
-  if stage == 1 || stage == 2    
-    if totalLoad2Shed > lastShed + 0.1
-      stage = 2;
-      toShed = lastShed + 0.1;
-      lastShed = toShed;
-      fprintf('[%10.2f] Intermediate stage: %f\n', t, toShed)
-      return
-    else
-      stage = 3;
-      toShed = totalLoad2Shed;
-      lastShed = toShed;
-      fprintf('[%10.2f] Final stage: %f\n', t, toShed)
-      return
-    end
-  end
+  return
 end
 
-toShed = lastShed;
+% The following will only be executed if timer == 0
+if deltaf <= -0.4
+  lastUnstableTime = t;
+  if deltaPest + deltaPsafe > 0 % deltaPest > 0 implies delta f < 0
+    timer = 1;
+    scheduledShed = deltaPest + deltaPsafe;
+    toShed = shedSoFar;
+    fprintf('[%8.2f] Scheduled level 1 %f + %f\n', t, shedSoFar, scheduledShed)
+    return
+  end
+elseif -0.4 < deltaf && deltaf <= -0.35
+  lastUnstableTime = t;
+  if deltaPest + deltaPsafe > 0 % deltaPest > 0 implies delta f < 0
+    timer = 2;
+    scheduledShed = deltaPest + deltaPsafe;
+    toShed = shedSoFar;
+    fprintf('[%8.2f] Scheduled level 2 %f + %f\n', t, shedSoFar, scheduledShed)
+    return
+  end
+elseif shedSoFar > 0 && t - lastUnstableTime >= 30
+  shedSoFar = 0;
+  toShed = 0;
+  fprintf('[%8.2f] Load reconnected\n', t)
+  return
+end
+
+toShed = shedSoFar;
