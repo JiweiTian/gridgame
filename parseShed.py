74ad06c5a27b346628a57af4c1ebd2c18e65a466
Yee Wei Law <ywlaw@unimelb.edu.au>
2012-06-15 22:06:43 +1000

    Added plotting using Matplotlib.

--- parseShed.py
+++ parseShed.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 import sys, os, re
+from pylab import *
 
 injectList = []
 shedList = []
@@ -26,13 +27,13 @@ while line != '':
 	m = injectPattern.search(line)
 	if m is not None:
 		#print m.group(0)+'-->'+m.group(1)
-		injectList.append(m.group(1))
+		injectList.append(float(m.group(1)))
 		shedList.append(0)
 	else:
 		m = shedPattern.search(line)
 		if m is not None:
 			#print m.group(0)+'-->'+m.group(1)
-			shedList[-1] = m.group(1)
+			shedList[-1] = float(m.group(1))
 	line = f.readline()
 
 # clean-up
@@ -41,5 +42,30 @@ f.close()
 # check result
 injectList.pop()	# remove potentially incomplete data due to termination of simulation
 shedList.pop()
-for inject, shed in zip(injectList, shedList):
-	print '%s\t\t%s' % (inject, shed)
+injectShedList = zip(injectList, shedList)
+injectShedList.sort()
+#for inject, shed in injectShedList:
+#	print '%s,\t%s' % (inject, shed)
+
+# statistical processing
+it = iter(injectShedList)
+freqBins = arange(-3, 0, 0.05)
+totalCnt = 0
+shedCnt = 0
+probList = []
+for freq in freqBins:
+	try:
+		(inject, shed) = it.next()
+		while freq <= inject and inject < freq + 0.05:
+			totalCnt += 1
+			if shed > 0:
+				shedCnt += 1
+			(inject, shed) = it.next()
+		probList.append(float(shedCnt)/totalCnt)
+		totalCnt = 0
+		shedCnt = 0
+	except Exception as exc:
+		probList.append(float(shedCnt)/totalCnt)
+		break
+plot(freqBins+([0.025]*len(freqBins)), probList) # take the middle of each freqBin
+show()
