701bcc6ba9f838731aedd8ea729ff3f5d86ba5e2
Yee Wei Law <ywlaw@unimelb.edu.au>
2012-06-15 15:29:47 +1000

    Added load-reconnecting and logging ability.

--- UFLS.m
+++ UFLS.m
@@ -2,13 +2,14 @@
 % Sampling time of this Simulink block should be set to 0.05s.
 function toShed = UFLS(t, deltaf, deltaPest)
 
-global deltaPsafe
-persistent timer shedSoFar scheduledShed lastUnstableTime
+global deltaPsafe logFile
+persistent timer shedSoFar scheduledShed shedStack lastUnstableTime
 
 if t <= 100
   timer = 0;
   shedSoFar = 0;
   scheduledShed = 0;
+  shedStack = [];
   lastUnstableTime = 0;
   toShed = 0;
   return
@@ -19,7 +20,8 @@ if timer > 0
   if timer == 0
     shedSoFar = shedSoFar + scheduledShed;
     toShed = shedSoFar;
-    fprintf('[%8.2f] Shed %f\n', t, toShed)
+    shedStack = [scheduledShed; shedStack];
+    fprintf(logFile, '[%9.2f] Shed %f\n', t, toShed);
   else
     toShed = shedSoFar;
   end
@@ -33,7 +35,7 @@ if deltaf <= -0.4
     timer = 1;
     scheduledShed = deltaPest + deltaPsafe;
     toShed = shedSoFar;
-    fprintf('[%8.2f] Scheduled level 1 %f + %f\n', t, shedSoFar, scheduledShed)
+    fprintf(logFile, '[%9.2f] Scheduled level 1 %f + %f\n', t, shedSoFar, scheduledShed);
     return
   end
 elseif -0.4 < deltaf && deltaf <= -0.35
@@ -42,13 +44,19 @@ elseif -0.4 < deltaf && deltaf <= -0.35
     timer = 2;
     scheduledShed = deltaPest + deltaPsafe;
     toShed = shedSoFar;
-    fprintf('[%8.2f] Scheduled level 2 %f + %f\n', t, shedSoFar, scheduledShed)
+    fprintf(logFile, '[%9.2f] Scheduled level 2 %f + %f\n', t, shedSoFar, scheduledShed);
     return
   end
-elseif shedSoFar > 0 && t - lastUnstableTime >= 30
-  shedSoFar = 0;
-  toShed = 0;
-  fprintf('[%8.2f] Load reconnected\n', t)
+elseif shedSoFar > 0 && t - lastUnstableTime >= 10
+  fprintf(logFile, '[%9.2f] Load reconnected: %f - %f\n', t, shedSoFar, shedStack(1));
+  if numel(shedStack) == 1
+    shedSoFar = 0;
+    shedStack = [];
+  else
+    shedSoFar = shedSoFar - shedStack(1);
+    shedStack = shedStack(2:numel(shedStack));
+  end
+  toShed = shedSoFar;  
   return
 end
 
