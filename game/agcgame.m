e6c639921e2f77c36ccf78a2e23e7737b41ab221
Yee Wei Law <ywlaw@unimelb.edu.au>
2012-07-05 19:39:42 +1000

    Added result for reconnection time of 60s.

--- game/agcgame.m
+++ game/agcgame.m
@@ -17,31 +17,12 @@
 % alternative is right s.m, x*M
 %
 
-% Dataset 1 is for AGC sampling rate of 1s
-% Dataset 2 is for AGC sampling rate of 2s
-dataset = 2;
-alpha = 0.9; % discount factor
-zerogs0 = false;
+stabletime = 30; % number of seconds during which frequency is stable after load shedding
+samplingrate = 0.5; % AGC sampling rate
+alpha = 0.1; % discount factor
+zerogs0 = false; % zero game matrix for state s0
 
-if dataset == 1
-  % M(a10,d1)
-  M(:,:,1,1)=[13/21, 8/21; 7/64, 57/64];
-  % M(a10,d2)
-  M(:,:,1,2)=[3/8,   5/8;  4/77, 73/77];
-  % M(a20,d1)
-  M(:,:,2,1)=[3/9,   6/9;  6/76, 70/76];
-  % M(a20,d2)
-  M(:,:,2,2)=[5/11,  6/11; 8/74, 66/74];
-  
-  % state s0: G(s0) = [a10,d1 a10,d2; a20,d1 a20,d2]
-  if ~zerogs0
-    G(:,:,1)=[-8.7975, -8.7984; -8.7787, -8.7652];
-  else
-    G(:,:,1)=[0, 0; 0, 0];
-  end
-  % state s1: G(s1) = [a10,d1 a10,d2; a20,d1 a20,d2]
-  G(:,:,2)=[ 0.3046,  0.3473;  0.3719,  0.3505];
-elseif dataset == 2
+if stabletime == 30 && samplingrate == 0.5
   % M(a10,d1)
   M(:,:,1,1)=[7/11, 4/11; 4/31, 27/31];
   % M(a10,d2)
@@ -52,15 +33,56 @@ elseif dataset == 2
   M(:,:,2,2)=[7/10, 3/10; 3/32, 29/32];
   
   % state s0: G(s0) = [a10,d1 a10,d2; a20,d1 a20,d2]
-  if ~zerogs0
-    G(:,:,1)=[-8.7894, -8.7672; -8.7606, -8.7602];
-  else
-    G(:,:,1)=[0, 0; 0, 0];
-  end
+  G(:,:,1)=[-8.7894, -8.7672; -8.7606, -8.7602];
   % state s1: G(s1) = [a10,d1 a10,d2; a20,d1 a20,d2]
   G(:,:,2)=[ 0.5038,  0.5884;  0.6643,  0.6450];
+elseif stabletime == 30 && samplingrate == 1
+  % M(a10,d1)
+  M(:,:,1,1)=[13/21, 8/21; 7/64, 57/64];
+  % M(a10,d2)
+  M(:,:,1,2)=[3/8,   5/8;  4/77, 73/77];
+  % M(a20,d1)
+  M(:,:,2,1)=[3/9,   6/9;  6/76, 70/76];
+  % M(a20,d2)
+  M(:,:,2,2)=[5/11,  6/11; 8/74, 66/74];
+  
+  % state s0: G(s0) = [a10,d1 a10,d2; a20,d1 a20,d2]
+  G(:,:,1)=[-8.7975, -8.7984; -8.7787, -8.7652];
+  % state s1: G(s1) = [a10,d1 a10,d2; a20,d1 a20,d2]
+  G(:,:,2)=[ 0.3046,  0.3473;  0.3719,  0.3505];
+elseif stabletime == 60 && samplingrate == 0.5
+  % M(a10,d1)
+  M(:,:,1,1)=[2/4  2/4; 0/38  38/38];
+  % M(a10,d2)
+  M(:,:,1,2)=[0/1  1/1; 0/41  41/41];
+  % M(a20,d1)
+  M(:,:,2,1)=[0/2  2/2; 1/40  39/40];
+  % M(a20,d2)
+  M(:,:,2,2)=[2/4  2/4; 2/38  36/38];
+  
+  % state s0: G(s0) = [a10,d1 a10,d2; a20,d1 a20,d2]
+  G(:,:,1)=[-8.7547 -8.8519; -8.7587 -8.7484];
+  % state s1: G(s1) = [a10,d1 a10,d2; a20,d1 a20,d2]
+  G(:,:,2)=[0.6778 0.7133; 0.6234 0.6132];
+elseif stabletime == 60 && samplingrate == 1
+  % M(a10,d1)
+  M(:,:,1,1)=[3/7  4/7; 2/78  76/78];
+  % M(a10,d2)
+  M(:,:,1,2)=[0/1  1/1; 0/84  84/84];
+  % M(a20,d1)
+  M(:,:,2,1)=[0/1  1/1; 0/84  84/84];
+  % M(a20,d2)
+  M(:,:,2,2)=[1/3  2/3; 1/82  81/82];
+  
+  % state s0: G(s0) = [a10,d1 a10,d2; a20,d1 a20,d2]
+  G(:,:,1)=[-8.8054 -8.8063; -8.8157 -8.7233];
+  % state s1: G(s1) = [a10,d1 a10,d2; a20,d1 a20,d2]
+  G(:,:,2)=[0.2722 0.3529; 0.4037 0.4081];
 end
 
+if zerogs0
+  G(:,:,1)=[0, 0; 0, 0];
+end
 
 [sim,cpu_time] = newsecgamesolve2(M,G,alpha);
 %[dummy,x,y]=zerosumgamesolver1(G(:,:,1)); %no vuln
@@ -96,7 +118,7 @@ bar([sim.policy1 sim.policy2])
 xlabel('Actions')
 set(gca,'XTickLabel',{'a10,d1','a20,d2'})
 ylabel('Probability')
-legend({'Attacker','Defender'},'location','north')
+legend({'Attacker','Defender'},'location','best')
 set(gcf, 'units', 'normalized')
 set(gcf, 'position', [0.8, 0.7, 0.15, 0.2])
 set(gca, 'YGrid', 'on')
