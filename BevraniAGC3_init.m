6177a4fdcac216a13b5ecfd8d21ff75526248fa5
Yee Wei Law <ywlaw@unimelb.edu.au>
2012-07-30 13:24:28 +1000

    Improved UFLS, added attacker and defender, and log file.

--- BevraniAGC3_init.m
+++ BevraniAGC3_init.m
@@ -1,6 +1,6 @@
 % Values from Bevrani's "Robst Power System Frequency Control," page 24,
 % Table 2.2
-K1 = 0.3;       K2 = 0.2;       K3 = 0.4;
+K_AGC1 = 0.3;   K_AGC2 = 0.2;   K_AGC3 = 0.4;
 D1 = 0.015;     D2 = 0.016;     D3 = 0.015;
 H1 = 0.1667/2;  H2 = 0.2017/2;  H3 = 0.1247/2;
 R1 = 3.00;      R2 = 2.73;      R3 = 2.82;
@@ -17,5 +17,25 @@ load('demand1.mat')
 load('demand2.mat')
 
 % Underfrequency load shedding
-global deltaPsafe
+global deltaPsafe aMaxShed aShedSoFar uflsInited
 deltaPsafe = -0.3/R1;
+aMaxShed = [4, 1];
+aShedSoFar = [0, 0];
+uflsInited = false;
+
+% Attack and defense
+global N currMeter nSamples flaggedMeter attackerInited defenderInited 
+N = 20; % a meter supplies N samples in a session
+currMeter = 1; set_param('BevraniAGC2/Attacker/Meter switch', 'sw', '1');
+nSamples = 1;
+flaggedMeter = 0;
+attackerInited = false;
+defenderInited = false;
+
+% Log file
+global logFile
+logFile = fopen('log.txt', 'w+');
+
+% Misc
+rng(1234);
+tic;
