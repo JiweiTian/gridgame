a30de434dd3c236a8347f44cc5c0cacc73c17a1b
Yee Wei Law <ywlaw@unimelb.edu.au>
2012-09-14 15:38:34 +1000

    Rewritten.

--- attacker_statcom.m
+++ attacker_statcom.m
@@ -1,25 +1,32 @@
-function vout = attacker_statcom(t, vina, vinb, vinc)
+function out = attacker_statcom(id, t, vina, vinb, vinc)
 
-global attackerInited
-persistent k b howLongToHack timeout
+global attackerInited hacked currMeter
+persistent kvec b howLongToHack timer
 
 if ~attackerInited
-  k = 1.2;
+  kvec = [1.1];
   b = 0;
-  howLongToHack = 0.1;
-  timeout = -1;
+  howLongToHack = 0.2;
+  timer = -1;
   attackerInited = true;
 end
 
-vout = k*[vina, vinb, vinc] + b*ones(1,3);
-
-if get_param('dstatcom_avg/game/meter_switch', 'sw') == '0' 
-  if timeout < 0
-    fprintf('t=%f: start hacking\n', t);
-    timeout = t + howLongToHack;
-  elseif t >= timeout
-    fprintf('t=%f: Hacked!\n', t);
-    set_param('dstatcom_avg/game/meter_switch', 'sw', '1');
-    timeout = -1;
+if currMeter == id && ~hacked(id)
+  if timer < 0
+    % start hacking
+    timer = t + howLongToHack;
+    fprintf('[%.4f] start hacking %d\n', t, id);
+  elseif timer > 0 && t >= timer
+    % finish hacking
+    timer = -1;
+    hacked(id) = true;
+    fprintf('[%.4f] finish hacking %d\n', t, id);
   end
+end
+
+if hacked(id)
+  vout = kvec(randi(numel(kvec),1))*[vina, vinb, vinc] + b*ones(1,3);
+  out = [vout, 1];
+else
+  out = [vina, vinb, vinc, 0];
 end
