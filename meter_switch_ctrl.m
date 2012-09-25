a1aeba307a196f5885e181e361dd50f5d8734bab
Yee Wei Law <ywlaw@unimelb.edu.au>
2012-09-25 22:12:07 +1000

    Supports reconnection and display of false positives.

--- meter_switch_ctrl.m
+++ meter_switch_ctrl.m
@@ -3,53 +3,53 @@
 function out = meter_switch_ctrl(t, loadShed)
 
 global meterSwitchInited attDetected currMeter hacked
-persistent transTime transTimer howLongToDisinfect disinfectTimer
+persistent howLongToTrans transTimer
+persistent howLongToDisinfect disinfectTimer
+persistent howLongToReconn reconnTimer
 
 if ~meterSwitchInited
-  transTime = 0.1;
+  howLongToTrans = 0.1;
   transTimer = -1;
-  howLongToDisinfect = 1;
+  howLongToDisinfect = 0.5;
+  % howLongToDisinfect = 1;
   disinfectTimer = -1;
+  howLongToReconn = 0.1;
+  reconnTimer = -1;
   meterSwitchInited = true;
 end
 
 if attDetected
-  transTimer = t + transTime;
-  disinfectTimer = transTimer + howLongToDisinfect;
-  if loadShed
-    fprintf('[%.4f] start ditching and disinfecting %d, and reconnecting load\n', t, currMeter)
-  else
-    fprintf('[%.4f] start ditching and disinfecting %d\n', t, currMeter)
-  end
+  transTimer = t + howLongToTrans;
+  fprintf('[%.4f] %d: begin ->%d\n', t, currMeter, mod(currMeter,2)+1)
   attDetected = false;
 end
 
-% finish transitioning to the other meter
-if transTimer > 0 && t >= transTimer
-  otherMeter = mod(currMeter,2)+1;
-  if hacked(otherMeter)
-    transTimer = transTimer + transTime;
-    fprintf('[%.4f] %d is not ready\n', t, otherMeter)
-  else
-    transTimer = -1;
-    if loadShed
-      fprintf('[%.4f] meter %d->%d, load reconnected\n', t, currMeter, otherMeter)
-    else
-      fprintf('[%.4f] meter %d->%d\n', t, currMeter, otherMeter)
-    end
-    currMeter = otherMeter;
-  end
+% done reconnecting load
+if reconnTimer > 0 && t >= reconnTimer
+  reconnTimer = -1;
 end
 
-% finish disinfecting
+% done disinfecting
 if disinfectTimer > 0 && t >= disinfectTimer  
   disinfectTimer = -1;
   disinfected = mod(currMeter,2)+1;
   hacked(disinfected) = false;
-  fprintf('[%.4f] done disinfecting %d\n', t, disinfected);
+  fprintf('[%.4f] %d: done disinfecting %d, begin reconn load\n', t, currMeter, disinfected);
+  reconnTimer = t + howLongToReconn; % it's only meaningful by now
 end
 
-whichPort = currMeter;
-loadRecon = (transTimer > 0)*1;
+% done transitioning to the other meter
+if transTimer > 0 && t >= transTimer
+  otherMeter = mod(currMeter,2)+1;
+  if hacked(otherMeter)
+    transTimer = transTimer + howLongToTrans;
+    fprintf('[%.4f] %d: %d is not ready\n', t, currMeter, otherMeter)
+  else
+    transTimer = -1;
+    disinfectTimer = t + howLongToDisinfect;  
+    fprintf('[%.4f] %d: done ->%d, begin disinfecting %d\n', t, otherMeter, otherMeter, currMeter)
+    currMeter = otherMeter;
+  end
+end
 
-out = [whichPort, loadRecon];
+out = [currMeter, (loadShed && reconnTimer > 0), (~hacked(mod(currMeter,2)+1) && disinfectTimer > 0)];
